b77bda62fb62906102332c339a2522d3be732abb
Manish kumar <krmanish244115@gmail.com>
2025-05-20 09:08:29 -0700

    Increment decrement Question push

---
+++ src/ex_Daily_task_Work/Lab5_Post_inre.java
@@ -0,0 +1,10 @@
+package ex_Daily_task_Work;
+
+public class Lab5_Post_inre {
+    public static void main(String[] args) {
+        int i = 11;
+
+        i = i++ + ++i;
+        System.out.println(i);
+    }
+}
