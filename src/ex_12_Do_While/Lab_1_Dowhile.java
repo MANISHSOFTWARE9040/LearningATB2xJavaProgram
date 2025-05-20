3e1b1ad64b2fe23099b1a5e6805a650f7ecc4538
Manish kumar <krmanish244115@gmail.com>
2025-05-20 09:08:29 -0700

    Function and Do while progam

---
+++ src/ex_12_Do_While/Lab_1_Dowhile.java
@@ -0,0 +1,14 @@
+package ex_12_Do_While;
+
+public class Lab_1_Dowhile {
+    public static void main(String[] args) {
+        int a = 1;
+        do {
+            System.out.println("Body!");
+            System.out.println(a);
+            a++;
+        } while (a < 10);
+
+
+    }
+}
---
+++ src/ex_12_Do_While/Lab_2_Dowhile.java
@@ -0,0 +1,16 @@
+package ex_12_Do_While;
+
+public class Lab_2_Dowhile {
+    public static void main(String[] args) {
+        int a = 0;
+//        while (a < 0) {
+//            System.out.println(a);
+//            a++;
+//        }
+
+        do{
+            System.out.println(a);
+            a++;
+        }while (a<0);
+    }
+}
